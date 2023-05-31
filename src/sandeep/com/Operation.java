92e45adcce3bcb94dd726843b5c99a10f6c133e9
SUDANABOINASANDEEP <sudanaboinakumar45@gmail.com>
2023-06-01 00:36:08 +0530

    this is my second project

---
+++ src/sandeep/com/Operation.java
@@ -0,0 +1,13 @@
+package sandeep.com;
+
+public class Operation {
+
+	public static void main(String[] args) {
+        int x=100;
+        int y=200;
+        int result;
+        result=x+y;
+        System.out.println(result);
+	}
+
+}
